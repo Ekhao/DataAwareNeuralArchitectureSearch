4a4001c189d51ea8b99dadaf482b8da171a29a6c
Emil Njor <emil.njor@gmail.com>
2022-08-23 15:53:02 +0200

    improved generality of search space

--- NAS.py
+++ NAS.py
@@ -5,9 +5,9 @@ import itertools
 from random import sample
 
 class SearchSpace():
-    def __init__(self) -> None:
-        self.input_search_space = self.search_space_gen([48000, 24000, 12000, 6000, 3000, 1500, 750, 325],["spectrogram", "mfe", "mfcc"],[32, 16, 8, 4, 2])
-        self.model_layer_search_space = self.search_space_gen([8,16,32,64,128],["relu", "tanh", "sigmoid", "elu"])
+    def __init__(self, model_layer_search_space, input_search_space) -> None:
+        self.input_search_space = self.search_space_gen(*input_search_space)
+        self.model_layer_search_space = self.search_space_gen(*model_layer_search_space)
 
 
     def search_space_gen(self, *iterables) -> dict:
@@ -18,7 +18,7 @@ class SearchSpace():
 
         return dict(zip(keys, values))
 
-search_space = SearchSpace()
+search_space = SearchSpace(model_layer_search_space=([8,16,32,64,128],["relu", "tanh", "sigmoid", "elu"]),input_search_space=([48000, 24000, 12000, 6000, 3000, 1500, 750, 325],["spectrogram", "mfe", "mfcc"],[32, 16, 8, 4, 2]))
 print(search_space.model_layer_search_space, search_space.input_search_space)
 
 
