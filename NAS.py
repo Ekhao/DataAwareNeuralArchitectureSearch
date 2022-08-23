20fdea5a6ffce70e03a62ed411d485259186a5f1
Emil Njor <emil.njor@gmail.com>
2022-08-23 15:47:10 +0200

    created initial search space generator

---
+++ NAS.py
@@ -0,0 +1,29 @@
+# Should we start with only searching for CNN architectures? We can use the binary encoding learnt from reading some papers in that case.
+# Or the more general approach from paperspace?
+
+import itertools
+from random import sample
+
+class SearchSpace():
+    def __init__(self) -> None:
+        self.input_search_space = self.search_space_gen([48000, 24000, 12000, 6000, 3000, 1500, 750, 325],["spectrogram", "mfe", "mfcc"],[32, 16, 8, 4, 2])
+        self.model_layer_search_space = self.search_space_gen([8,16,32,64,128],["relu", "tanh", "sigmoid", "elu"])
+
+
+    def search_space_gen(self, *iterables) -> dict:
+
+        #Encode combinations in the search space into numbers using dictionaries
+        values = list(itertools.product(*iterables))
+        keys = [key for key in range(len(values))]
+
+        return dict(zip(keys, values))
+
+search_space = SearchSpace()
+print(search_space.model_layer_search_space, search_space.input_search_space)
+
+
+
+
+# Create sequences of configurations
+
+# Convert these into actual machine learning models
