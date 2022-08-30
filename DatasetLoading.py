3175d95f68c8e4aef195df7d2d44a5fbacf065ca
Emil Njor <emil.njor@gmail.com>
2022-08-30 15:32:00 +0200

    applied autoformatting

--- DatasetLoading.py
+++ DatasetLoading.py
@@ -3,15 +3,19 @@ import time
 import librosa
 import tensorflow as tf
 
+
 class DatasetLoader:
     def __init__(self) -> None:
         pass
     # There are 1800 normal files and 400 anomalous files (in channel 1)
-    def load_dataset(self, path_normal_files, path_anomalous_files,sample_rate, preprocessing_type,
-                     num_normal_files = 1800, num_anomalous_files = 400, channel = 1) -> tuple:
+
+    def load_dataset(self, path_normal_files, path_anomalous_files, sample_rate, preprocessing_type,
+                     num_normal_files=1800, num_anomalous_files=400, channel=1) -> tuple:
         # Find every channel one file in the listed directories
-        normal_files = tf.io.gfile.glob(f"{path_normal_files}*ch{channel}*.wav")
-        anomalous_files = tf.io.gfile.glob(f"{path_anomalous_files}*ch{channel}*.wav")
+        normal_files = tf.io.gfile.glob(
+            f"{path_normal_files}*ch{channel}*.wav")
+        anomalous_files = tf.io.gfile.glob(
+            f"{path_anomalous_files}*ch{channel}*.wav")
 
         # Cut the amount of processed files according to the program parameters
         normal_files = normal_files[:num_normal_files]
@@ -21,25 +25,31 @@ class DatasetLoader:
         sample_rate = librosa.get_samplerate(normal_files[0])
 
         # Load the audio files using librosa. Use multiple python processes in order to speed up loading.
-        normal_audio = Parallel(n_jobs=-1)(delayed(librosa.load)(file, sr=sample_rate) for file in normal_files)
-        anomalous_audio = Parallel(n_jobs=-1)(delayed(librosa.load)(file, sr=sample_rate) for file in anomalous_files)
+        normal_audio = Parallel(
+            n_jobs=-1)(delayed(librosa.load)(file, sr=sample_rate) for file in normal_files)
+        anomalous_audio = Parallel(
+            n_jobs=-1)(delayed(librosa.load)(file, sr=sample_rate) for file in anomalous_files)
 
         match preprocessing_type:
             case "waveform":
                 return (normal_audio, anomalous_audio)
             case "spectrogram":
-                raise NotImplementedError("Loading data as a spectrogram has not been implemented yet")
+                raise NotImplementedError(
+                    "Loading data as a spectrogram has not been implemented yet")
             case "mel-spectrogram":
-                raise NotImplementedError("Loading data as a mel-spectrogram has not been implemented yet")
+                raise NotImplementedError(
+                    "Loading data as a mel-spectrogram has not been implemented yet")
             case "mfcc":
-                raise NotImplementedError("Loading data as a MFCC has not been implemented yet")
+                raise NotImplementedError(
+                    "Loading data as a MFCC has not been implemented yet")
 
         # Things to vary: sample rate, mfcc-spectrogram-melspectrogram-rawwaveform, bit-width-representation. Neural network architecture
         # I think that it makes sense to vary this as a part of the NAS file when the architecture is constructed based on the genotype.
 
-#Stupid_testing
+
+# Stupid_testing
 dataset_loader = DatasetLoader()
-loaded_dataset = dataset_loader.load_dataset(path_normal_files="/Users/emjn/Documents/DTU/Datasets/ToyConveyor/case1/NormalSound_IND/", 
+loaded_dataset = dataset_loader.load_dataset(path_normal_files="/Users/emjn/Documents/DTU/Datasets/ToyConveyor/case1/NormalSound_IND/",
                                              path_anomalous_files="/Users/emjn/Documents/DTU/Datasets/ToyConveyor/case1/AnomalousSound_IND/",
                                              sample_rate=48000, preprocessing_type="waveform", num_normal_files=100, num_anomalous_files=20)
 print(loaded_dataset)
--- NAS.py
+++ NAS.py
@@ -6,23 +6,27 @@ import DatasetLoading
 
 NUM_OUTPUT_CLASSES = 2
 LOSS_FUNCTION = tf.keras.losses.CategoricalCrossentropy
-MODEL_LAYER_SEARCH_SPACE = ([8,16,32,64,128],[3,5],["relu", "sigmoid"]) # Number of filters, filter size and activation function
-INPUT_SEARCH_SPACE = ([48000, 24000, 12000, 6000, 3000, 1500, 750, 325],["spectrogram", "mfe", "mfcc", "waveform"]) # Sample rate and preprocessing type - maybe also add bit width
+# Number of filters, filter size and activation function
+MODEL_LAYER_SEARCH_SPACE = ([8, 16, 32, 64, 128], [3, 5], ["relu", "sigmoid"])
+INPUT_SEARCH_SPACE = ([48000, 24000, 12000, 6000, 3000, 1500, 750, 325], [
+                      "spectrogram", "mfe", "mfcc", "waveform"])  # Sample rate and preprocessing type - maybe also add bit width
+
 
 class SearchSpace:
     def __init__(self, model_layer_search_space, input_search_space) -> None:
         self.input_search_space = self.search_space_gen(*input_search_space)
-        self.model_layer_search_space = self.search_space_gen(*model_layer_search_space) # Maybe add dropout and output layers to the model search space
-
+        # Maybe add dropout and output layers to the model search space
+        self.model_layer_search_space = self.search_space_gen(
+            *model_layer_search_space)
 
     def search_space_gen(self, *iterables) -> dict:
 
-        #Encode combinations in the search space into numbers using dictionaries
+        # Encode combinations in the search space into numbers using dictionaries
         values = list(itertools.product(*iterables))
         keys = [key for key in range(len(values))]
 
         return dict(zip(keys, values))
-    
+
     def input_decode(self, number: int) -> tuple:
         return self.input_search_space[number]
 
@@ -30,29 +34,33 @@ class SearchSpace:
         decoded_sequence = []
         for key in sequence:
             decoded_sequence.append(self.model_layer_search_space[key])
-        
+
         return decoded_sequence
 
+
 class InputModelGenerator:
-    def __init__(self, target_classes, loss_function, optimizer = tf.keras.optimizers.Adam(learning_rate = 0.001), dropout_rate = 0.5, metrics = ["accuracy"]):
+    def __init__(self, target_classes, loss_function, optimizer=tf.keras.optimizers.Adam(learning_rate=0.001), dropout_rate=0.5, metrics=["accuracy"]):
         self.target_classes = target_classes
         self.loss_function = loss_function
         self.optimizer = optimizer
         self.dropout_rate = dropout_rate
         self.metrics = metrics
-        self.search_space = SearchSpace(model_layer_search_space=MODEL_LAYER_SEARCH_SPACE,input_search_space=INPUT_SEARCH_SPACE)
-    
-    def create_input_model(self, sequence: list[int], input_shape = tuple) -> tf.keras.Model:
+        self.search_space = SearchSpace(
+            model_layer_search_space=MODEL_LAYER_SEARCH_SPACE, input_search_space=INPUT_SEARCH_SPACE)
+
+    def create_input_model(self, sequence: list[int], input_shape=tuple) -> tf.keras.Model:
         layer_configs = self.search_space.model_decode(sequence)
 
         model = tf.keras.Sequential()
 
         # For the first layer we need to define the input shape
-        model.add(tf.keras.layers.Conv2D(filters = layer_configs[0][0], kernel_size = layer_configs[0][1], activation = layer_configs[0][2], input_shape=input_shape))
+        model.add(tf.keras.layers.Conv2D(
+            filters=layer_configs[0][0], kernel_size=layer_configs[0][1], activation=layer_configs[0][2], input_shape=input_shape))
 
         for layer_config in layer_configs[1:]:
-            model.add(tf.keras.layers.Conv2D(filters = layer_config[0], kernel_size = layer_config[1], activation = layer_config[2]))
-        
+            model.add(tf.keras.layers.Conv2D(
+                filters=layer_config[0], kernel_size=layer_config[1], activation=layer_config[2]))
+
         # The standard convolutional model has dense layers at its end for classification - let us make the same assumption TODO: should be a part of search space
         model.add(tf.keras.layers.Flatten())
 
@@ -61,11 +69,12 @@ class InputModelGenerator:
         # Output layer
         model.add(tf.keras.layers.Dense(self.target_classes))
 
-        model.compile(optimizer=self.optimizer, loss=self.loss_function, metrics=self.metrics)
+        model.compile(optimizer=self.optimizer,
+                      loss=self.loss_function, metrics=self.metrics)
 
         return model
 
 
 model_generator = InputModelGenerator(NUM_OUTPUT_CLASSES, LOSS_FUNCTION)
-model = model_generator.create_input_model([6,8,2], (32,32,3))
+model = model_generator.create_input_model([6, 8, 2], (32, 32, 3))
 print(model.summary())
