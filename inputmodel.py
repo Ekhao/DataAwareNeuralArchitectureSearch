1d2bd36a9e32b86cf946f550faf6cc4860f5a0eb
Emil Njor <emil.njor@gmail.com>
2022-12-12 10:23:13 +0100

    fixed models of different processes being saved under the same name and catching of obscure tflite bugs

--- inputmodel.py
+++ inputmodel.py
@@ -6,6 +6,7 @@ import sklearn.metrics
 import numpy as np
 import pathlib
 import struct
+import random
 
 
 class InputModel:
@@ -119,17 +120,25 @@ class InputModel:
         return
 
     def __evaluate_model_size(self):
+        unique_extension = random.randint(0, 10000000)
         save_directory = pathlib.Path("./tmp/")
-        tf_model_file = save_directory/"tf_model"
+        tf_model_file = save_directory/f"tf_model-{unique_extension}"
         tf.saved_model.save(self.model, tf_model_file)
 
-        converter = tf.lite.TFLiteConverter.from_saved_model(
-            tf_model_file.resolve().as_posix())
+        try:
+            converter = tf.lite.TFLiteConverter.from_saved_model(
+                tf_model_file.resolve().as_posix())
+        except Exception as e:
+            print(
+                f"{e}\nSaving model size as max size + 1 (2000000001) for this to be identified later.")
         try:
             tflite_model = converter.convert()
         except struct.error:
             return 2000000000
+        except Exception as e:
+            print(
+                f"{e}\nSaving model size as max size + 1 (2000000001) for this to be identified later.")
 
-        tflite_model_file = save_directory/"tflite_model"
+        tflite_model_file = save_directory/f"tflite_model-{unique_extension}"
         model_size = tflite_model_file.write_bytes(tflite_model)
         return model_size
