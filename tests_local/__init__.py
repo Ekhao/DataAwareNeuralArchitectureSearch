cff368fcb294052c8065e3bfd48e5b661782a2a9
Emil Njor <emil.njor@gmail.com>
2023-04-18 14:29:57 +0200

    changed test file structure to support local and cloud test differences

---
+++ tests_local/__init__.py
@@ -0,0 +1,2 @@
+# This is an empty file required for pytest to find tests in this subfolder
+# The tests_local folder is meant for tests that can not be run on the CI e.g. because of large dataset dependencies.
