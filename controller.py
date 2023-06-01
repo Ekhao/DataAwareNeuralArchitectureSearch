e5bbaf94a451fe7319ee710e6280a8dfb8cc8ddc
Emil Njor <emil.njor@gmail.com>
2023-06-01 12:43:41 +0200

    introduced types to controller

--- controller.py
+++ controller.py
@@ -2,20 +2,24 @@
 
 # Standard Library Imports
 from abc import ABC, abstractmethod
+from typing import Any
 
 # Local Imports
 from searchspace import SearchSpace
+from datamodel import DataModel
+
+Configuration = tuple[tuple[Any, ...], list[tuple[Any, ...]]]
+Individual = tuple[DataModel, float]
 
 
 class Controller(ABC):
-
     def __init__(self, search_space: SearchSpace) -> None:
         self.search_space = search_space
 
     @abstractmethod
-    def generate_configuration(self):
+    def generate_configuration(self) -> Configuration:
         pass
 
     @abstractmethod
-    def update_parameters(self, data_model):
+    def update_parameters(self, data_model: DataModel) -> None:
         pass
