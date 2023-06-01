1fcea2c716ad2d64b99f96e90b89f8b11bfc2559
Emil Njor <emil.njor@gmail.com>
2023-06-01 14:43:43 +0200

    updated controller inheritance

--- controller.py
+++ controller.py
@@ -2,19 +2,19 @@
 
 # Standard Library Imports
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import Any, Optional
 
 # Local Imports
 from searchspace import SearchSpace
 from datamodel import DataModel
 
 Configuration = tuple[tuple[Any, ...], list[tuple[Any, ...]]]
-Individual = tuple[DataModel, float]
 
 
 class Controller(ABC):
-    def __init__(self, search_space: SearchSpace) -> None:
+    def __init__(self, search_space: SearchSpace, seed: Optional[int] = None) -> None:
         self.search_space = search_space
+        self.seed = seed
 
     @abstractmethod
     def generate_configuration(self) -> Configuration:
